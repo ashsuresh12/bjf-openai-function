7abc343300ad9e3b252c29023c1140aa41cfe22a
ashsuresh12 <ash@incubatedigital.com.au>
2025-06-09 14:37:41 +1000

    Add files via upload

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ dotenv.config();
 const app = express();
 const BATCH_SIZE = 400;
 const SHEET_NAME = 'NewTagging';
-const TRACKER_CELL = 'Z1';
+const TRACKER_CELL = 'CG1';
 const OUTPUT_COLUMN = 'H';
 const SHEET_ID = process.env.SPREADSHEET_ID;
 
